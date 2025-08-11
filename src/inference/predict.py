147c6c91cd8133f745f1c486b8dfa4bea6179723
Mahsa Sinaei <mahsa.sinaei1991@gmail.com>
2025-08-11 15:13:36 -0400

    style: format code for improved readability

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -1,10 +1,12 @@
 from ultralytics import YOLO
+
 from inference.conf import MODEL_PATH
+
 
 class Predictor:
     def __init__(self, model_path=MODEL_PATH):
         self.model = YOLO(model_path)
-    
+
     def predict(self, img_path):
         results = self.model(img_path, verbose=False)
         boxes = results[0].boxes
@@ -17,10 +19,12 @@ class Predictor:
             xywh = box.xywh[0].tolist()
             class_boxes.setdefault(cls_name, []).append(xywh)
 
-        return class_boxes  
+        return class_boxes
 
 
 if __name__ == "__main__":
     pred = Predictor()
-    output = pred.predict("volume_1_slice_100_jpg.rf.b3e873b1f4d54ee6c59a12a7b5e3bdcb.jpg")
+    output = pred.predict(
+        "volume_1_slice_100_jpg.rf.b3e873b1f4d54ee6c59a12a7b5e3bdcb.jpg"
+    )
     print(output)
